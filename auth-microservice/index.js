9bb5f4a2a4959d771357e4395027d03c635f8150
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2025-07-08 15:01:52 +0200

    docker

--- auth-microservice/index.js
+++ auth-microservice/index.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(express.json());
 app.use("/", authRoutes);
 
-// Connexion DB + lancement serveur
 connectDB().then(() => {
   app.listen(process.env.PORT, () => {
     console.log(`Auth service running on port ${process.env.PORT}`);
